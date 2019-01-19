ef6dcb2fec83d253f19fc69df807d8f45cc7a5cd
Shubham Agrawal <shubham@edtal.com>
2019-01-19 18:19:28 +0530

    Added Nav menu in the landing page

--- js/main.js
+++ js/main.js
@@ -292,3 +292,21 @@ particlesJS("particles-js",
     resetCallback: function() {}
   }
 }(window, document, window.jQuery);
+
+function burger(){
+  var burger = document.getElementById('burger');
+  var links = document.getElementById('links');
+  var quit = document.getElementById('quit');
+  burger.style.padding = '16px 16px 200vw 200vw';
+  links.style.display = 'flex';
+  quit.style.display = 'inline';
+}
+
+function quit(){
+  var burger = document.getElementById('burger');
+  var links = document.getElementById('links');
+  var quit = document.getElementById('quit');
+  burger.style.padding = '16px 16px 32px 32px';
+  links.style.display = 'none';
+  quit.style.display = 'none';
+}
